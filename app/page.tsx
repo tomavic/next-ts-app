d27d05be39c559a829c6ce50cb78363ab848c519
tomavic <hbasheer@live.com>
2024-09-16 23:44:09 +0300

    feat: use shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { EnvelopeOpenIcon } from "@radix-ui/react-icons";
+
 export default function Home() {
   return (
     <>
       <h1>Hola </h1>
       <p>I am Hatem</p>
+
+      <Button>
+        <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email
+      </Button>
     </>
   );
 }
